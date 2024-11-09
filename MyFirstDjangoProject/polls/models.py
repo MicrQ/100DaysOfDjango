f777574e93eb2309c3f64709a4eda632bf55b319
Abenet Gebre <mail.micrq@proton.me>
2024-11-09 15:44:40 +0300

    created Question and Choice models

--- MyFirstDjangoProject/polls/models.py
+++ MyFirstDjangoProject/polls/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+""" Model definitions for the polls app """
 
-# Create your models here.
+
+class Question(models.Model):
+    """ a model that represents a Question"""
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+
+class Choice(models.Model):
+    """ a model that represents user's Choice """
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
