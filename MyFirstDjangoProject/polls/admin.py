4e1f991e6bf1d41613070f9a8290f07281f6f425
Abenet Gebre <mail.micrq@proton.me>
2024-11-11 09:52:51 +0300

    created superuser(admin) to make use of the built-in admin dashboard

--- MyFirstDjangoProject/polls/admin.py
+++ MyFirstDjangoProject/polls/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Question
+""" a module used to register models to make
+    them available on the admin interface
+"""
 
-# Register your models here.
+
+admin.site.register(Question)
