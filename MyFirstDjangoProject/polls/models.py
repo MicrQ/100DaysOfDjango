1bce1cf7d5c7a41af7276776ada791d3e4d2cc90
Abenet Gebre <mail.micrq@proton.me>
2024-11-11 09:05:01 +0300

    added a string representation for models

--- MyFirstDjangoProject/polls/models.py
+++ MyFirstDjangoProject/polls/models.py
@@ -7,9 +7,17 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    def __str__(self):
+        """ string representation of the model """
+        return self.question_text
+
 
 class Choice(models.Model):
     """ a model that represents user's Choice """
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        """ string representation of the model """
+        return self.choice_text
