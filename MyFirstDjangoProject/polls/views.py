d6a464095262dd7a2ca61300af7948768056cac7
Abenet Gebre <mail.micrq@proton.me>
2024-11-11 09:54:20 +0300

    created more views for vote, result, and detail of a question.

--- MyFirstDjangoProject/polls/views.py
+++ MyFirstDjangoProject/polls/views.py
@@ -11,3 +11,42 @@ def index(request):
             HttpResponse with simple text
     """
     return HttpResponse("Welcome to polls app!")
+
+
+def detail(request, qustion_id: int):
+    """ handles the  detail of a question
+        Args:
+            request: HttpRequest object
+            question_id: the id of the question
+        Return:
+            HttpResponse(view): details of the specified question.
+    """
+    response = "You're looking at the results of question %s"
+
+    return HttpResponse(response % qustion_id)
+
+
+def results(request, question_id: int):
+    """ handles the results of a question
+
+        Args:
+            request: HttpRequest object
+            question_id (int): the id of the question
+        Return:
+            HttpResponse(view): results of the specified question.
+    """
+    response = "You're looking at the results of question %s"
+
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id: int):
+    """ handles the voting of a question
+
+        Args:
+            request: HttpRequest object
+            question_id: the id of a specific question
+        Return:
+            HttpResponse(view): voting page of the specified question.
+    """
+    return HttpResponse("You're voting on question %s." % question_id)
