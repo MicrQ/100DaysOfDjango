b251723db548f2361057953bd330c8062b024005
Abenet Gebre <mail.micrq@proton.me>
2024-11-15 19:42:35 +0300

    created a method for Question model. wrote a the first test for Question model

--- MyFirstDjangoProject/polls/models.py
+++ MyFirstDjangoProject/polls/models.py
@@ -1,3 +1,5 @@
+import datetime
+from django.utils import timezone
 from django.db import models
 """ Model definitions for the polls app """
 
@@ -10,6 +12,10 @@ class Question(models.Model):
     def __str__(self):
         """ string representation of the model """
         return self.question_text
+    
+    def was_published_recently(self):
+        """ returns True if the question was published recently """
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
 class Choice(models.Model):
