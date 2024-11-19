6c592c7719a585523b6146390eb07a97ff55513f
Abenet Gebre <mail.micrq@proton.me>
2024-11-19 16:30:54 +0300

    custom user model created

--- ch4-bookstore/accounts/models.py
+++ ch4-bookstore/accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomModel(AbstractUser):
+    """ represents a custom user model.
+        instead of using the default user model
+    """
+    pass
