0c4584e70b7a8644462df475bef79cd96e43536c
Abenet Gebre <mail.micrq@proton.me>
2024-11-19 16:52:00 +0300

    migration created. setting.py modified.

--- ch4-bookstore/accounts/models.py
+++ ch4-bookstore/accounts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
-class CustomModel(AbstractUser):
+class CustomUser(AbstractUser):
     """ represents a custom user model.
         instead of using the default user model
     """
