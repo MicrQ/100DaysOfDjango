87c20a0b408849fd4c61639bf850e23776ebb30d
Abenet Gebre <mail.micrq@proton.me>
2024-11-20 17:25:32 +0300

    (MODIFIED): more tests added for the signup page and its functionality

--- ch4-bookstore/accounts/tests.py
+++ ch4-bookstore/accounts/tests.py
@@ -1,5 +1,8 @@
+from django.urls import reverse, resolve
 from django.test import TestCase
+from .forms import CustomUserCreationForm
 from django.contrib.auth import get_user_model
+from .views import SignupPageView
 
 
 class CustomUserTests(TestCase):
@@ -32,3 +35,33 @@ class CustomUserTests(TestCase):
         self.assertTrue(admin_user.is_active)
         self.assertTrue(admin_user.is_staff)
         self.assertTrue(admin_user.is_superuser)
+
+
+class SignUpPageTests(TestCase):
+    """ tests dedicated to the signup page and functionality """
+
+    def setUp(self):
+        """ will be used to avoid redundency and repetition """
+        url = reverse('signup')
+        self.response = self.client.get(url)
+
+    def test_signup_template(self):
+        """ test to check if the right template is used """
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, 'registration/signup.html')
+        self.assertContains(self.response, 'Sign Up')
+        self.assertNotContains(self.response, 'Hi there! I should not be here.')
+
+    def test_signup_form(self):
+        """" test for the form used in the signup page """
+        form = self.response.context.get('form')
+        self.assertIsInstance(form, CustomUserCreationForm)
+        self.assertContains(self.response, 'csrfmiddlewaretoken')
+
+    def test_signup_view(self):
+        """ test for the signup page view """
+        view = resolve('/accounts/signup/')
+        self.assertEqual(
+            view.func.__name__,
+            SignupPageView.as_view().__name__
+        )
