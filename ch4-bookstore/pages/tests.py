43378fea25b537424826f1918815f86b8028a078
Abenet Gebre <mail.micrq@proton.me>
2024-11-20 11:21:34 +0300

    created tests for the home page templates

--- ch4-bookstore/pages/tests.py
+++ ch4-bookstore/pages/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class HomePageTests(SimpleTestCase):
+    """ tests for the home page templates """
+
+    def test_url_exists_at_correct_location(self):
+        """ test to check if the home url is working """
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_homepage_url_name(self):
+        """ test if the homepage's url name is pointing
+            to the right path || endpoint
+        """
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
