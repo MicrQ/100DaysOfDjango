1cdf507c0b8d7493cba14157211c312a15319751
Abenet Gebre <mail.micrq@proton.me>
2024-11-20 11:47:16 +0300

    (MODIFIED): created setUp method to avoid redundency in tests

--- ch4-bookstore/pages/tests.py
+++ ch4-bookstore/pages/tests.py
@@ -5,33 +5,33 @@ from django.urls import reverse
 class HomePageTests(SimpleTestCase):
     """ tests for the home page templates """
 
+    def setUp(self):
+        """ will be used to avoid redundency and repetition """
+        url = reverse('home')
+        self.response = self.client.get(url)
+
     def test_url_exists_at_correct_location(self):
         """ test to check if the home url is working """
-        response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.response.status_code, 200)
 
     def test_homepage_url_name(self):
         """ test if the homepage's url name is pointing
             to the right path || endpoint
         """
-        response = self.client.get(reverse('home'))
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.response.status_code, 200)
 
     def test_homepage_template(self):
         """ test to check if the right template is used """
-        response = self.client.get(reverse('home'))
-        self.assertTemplateUsed(response, 'home.html')
+        self.assertTemplateUsed(self.response, 'home.html')
 
     def test_homepage_contains_correct_html(self):
         """ test to check if the response contains
             the expected html
         """
-        response = self.client.get(reverse('home'))
-        self.assertContains(response, 'home page')
+        self.assertContains(self.response, 'home page')
 
     def test_homepage_does_not_contain_incorrect_html(self):
         """ test to check if the response does not contain
             the unexpected html
         """
-        response = self.client.get(reverse('home'))
-        self.assertNotContains(response, 'Hi there! I should not be here.')
+        self.assertNotContains(self.response, 'Hi there! I should not be here.')
