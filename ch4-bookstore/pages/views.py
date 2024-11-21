e05cfd6ff85d21c13a1545848998e32ee27afe96
Abenet Gebre <mail.micrq@proton.me>
2024-11-21 16:30:13 +0300

    (NEW): created a static about page.

--- ch4-bookstore/pages/views.py
+++ ch4-bookstore/pages/views.py
@@ -4,3 +4,8 @@ from django.views.generic import TemplateView
 class HomePageView(TemplateView):
     """ landing page """
     template_name = 'home.html'
+
+
+class AboutPageView(TemplateView):
+    """ view for the static about page """
+    template_name = 'about.html'
