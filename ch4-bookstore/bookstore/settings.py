e9552ea15be59caa56715880e0f4c5fc7a23d9cc
Abenet Gebre <mail.micrq@proton.me>
2024-11-25 15:20:04 +0300

    (MODIFIED): used environment variable to store the django secret key. stored it on docker-compose.yml file instead of hard coding it.

--- ch4-bookstore/bookstore/settings.py
+++ ch4-bookstore/bookstore/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-oq0bf%12p__36toq$y2ul#z6mi7tt@ph#t%^w3&p+0^js!-vdw'
+SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
