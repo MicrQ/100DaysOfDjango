89d3ad48d8b16a9ed590aca7c4787cb4a20f36fa
Abenet Gebre <mail.micrq@proton.me>
2024-11-25 09:46:25 +0300

    (MODIFIED): allauth middleware added. allauth urls replaced the django auth urls

--- ch4-bookstore/bookstore/settings.py
+++ ch4-bookstore/bookstore/settings.py
@@ -59,6 +59,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # allauth middleware
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'bookstore.urls'
