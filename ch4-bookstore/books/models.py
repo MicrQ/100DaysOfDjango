28f33dda06c2a7ded644b5e2086d56cdf3739425
Abenet Gebre <mail.micrq@proton.me>
2024-11-26 09:24:48 +0300

    (NEW): created a book class | model. created migrations for it

--- ch4-bookstore/books/models.py
+++ ch4-bookstore/books/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    """ book representation """
+
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        """ string representation of the model """
+        return self.title
