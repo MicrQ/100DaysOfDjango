3adf4beeb66f321f72ad19e2f3823c1df35360bf
Abenet Gebre <mail.micrq@proton.me>
2024-11-26 18:15:38 +0300

    (NEW): created review model and registered it to be displayed on the admin page

--- ch4-bookstore/books/admin.py
+++ ch4-bookstore/books/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, Review
+
+
+class ReviewInline(admin.TabularInline):
+    """ used to make the review be displayed on the admin page """
+    model = Review
 
 
 class BookAdmin(admin.ModelAdmin):
     """ used to specify which fields to be displayed on the admin page """
+    inlines = [
+        ReviewInline,
+    ]
     list_display = ('title', 'author', 'price')
 
 
--- ch4-bookstore/books/models.py
+++ ch4-bookstore/books/models.py
@@ -1,6 +1,7 @@
 import uuid
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth import get_user_model
 
 
 class Book(models.Model):
@@ -20,3 +21,22 @@ class Book(models.Model):
 
     def get_absolute_url(self): # new
         return reverse("book_detail", args=[str(self.id)])
+    
+
+class Review(models.Model):
+    """ represents user review of a book """
+
+    book = models.ForeignKey(
+        Book,
+        on_delete=models.CASCADE,
+        related_name='reviews')
+    
+    review = models.CharField(max_length=255)
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        """ string representation of the model """
+        return self.review
