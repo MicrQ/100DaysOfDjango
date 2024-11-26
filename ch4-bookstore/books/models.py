af274acf89ed958ef3b0cfedc707d444fbe2d7a2
Abenet Gebre <mail.micrq@proton.me>
2024-11-26 10:38:08 +0300

    (NEW): created a book detail view, its url, and template

--- ch4-bookstore/books/models.py
+++ ch4-bookstore/books/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Book(models.Model):
@@ -11,3 +12,6 @@ class Book(models.Model):
     def __str__(self):
         """ string representation of the model """
         return self.title
+
+    def get_absolute_url(self): # new
+        return reverse("book_detail", args=[str(self.id)])
--- ch4-bookstore/books/views.py
+++ ch4-bookstore/books/views.py
@@ -1,9 +1,17 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Book
 
 
 # Create your views here.
 class BookListView(ListView):
+    """ used to display give list of models """
     model = Book
     context_object_name = 'book_list'
     template_name = 'books/book_list.html'
+
+
+class BookDetailView(DetailView):
+    """ used to display the detail of a book model """
+    model = Book
+    context_object_name = 'book'
+    template_name = 'books/book_detail.html'
