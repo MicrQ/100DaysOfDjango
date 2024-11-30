c2d937566f2d9e19c307e8fab0c9f4033acd6c8f
Abenet Gebre <mail.micrq@proton.me>
2024-11-30 11:31:49 +0300

    wrote some tests for the books app

--- library/books/tests.py
+++ library/books/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from .models import Book
+from django.urls import reverse
 
-# Create your tests here.
+
+class BookTests(TestCase):
+    """ test for the books app and its functionalities """
+
+    @classmethod
+    def setUpTestData(cls):
+        """ setting up data to use for tests """
+
+        cls.book = Book.objects.create(
+            title='A good title',
+            subtitle='An excellent subtitle',
+            author='Tom',
+            isbn='32454657898',
+        )
+
+    def test_book_content(self):
+        """ test to check if the write data of the book is saved """
+        self.assertEqual(self.book.title, 'A good title')
+        self.assertEqual(self.book.subtitle, 'An excellent subtitle')
+        self.assertEqual(self.book.author, 'Tom')
+        self.assertEqual(self.book.isbn, '32454657898')
+
+    def test_book_list_view(self):
+        """ test to check the view of the books app """
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'An excellent subtitle')
+        self.assertTemplateUsed(response, 'books/book_list.html')
