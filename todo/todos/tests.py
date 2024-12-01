1052022ca031d1e422fcc8dea53578475064d37a
Abenet Gebre <mail.micrq@proton.me>
2024-12-01 22:37:34 +0300

    wrote tests for the todo model

--- todo/todos/tests.py
+++ todo/todos/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from .models import Todo
 
-# Create your tests here.
+
+class TodoModelTest(TestCase):
+    """ tests for the todo model """
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.todo = Todo.objects.create(
+            title='First Todo',
+            body='A body of the first todo'
+        )
+
+    def test_model_content(self):
+        """ tests to check the todo model contents """
+        self.assertEqual(self.todo.title, 'First Todo')
+        self.assertEqual(self.todo.body, 'A body of the first todo')
+        self.assertEqual(str(self.todo), 'First Todo')
