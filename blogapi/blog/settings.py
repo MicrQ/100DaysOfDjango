99aecc5c82bff9107189f7ea99e54b648c30fa53
Abenet Gebre <mail.micrq@proton.me>
2024-12-03 15:42:37 +0300

    installed and configured CORS. additionally, added CSRF protection

--- blogapi/blog/settings.py
+++ blogapi/blog/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'posts.apps.PostsConfig',
     # Third party
     'rest_framework',
+    'corsheaders',
 ]
 
 # Rest framework config
@@ -54,12 +55,20 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware', # CORS
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:8000',
+    'http://localhost:3000'
+)
+
+CSRF_TRUSTED_ORIGINS = ['http://localhost:3000']
 
 ROOT_URLCONF = 'blog.urls'
 
