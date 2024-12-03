f0b11be9428b0dd99061ebd5add1e626b454bb61
Abenet Gebre <mail.micrq@proton.me>
2024-12-03 14:28:36 +0300

    wrote a test for the post model

--- blogapi/posts/tests.py
+++ blogapi/posts/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from .models import Post
 
-# Create your tests here.
+
+class BlogTests(TestCase):
+    """ tests for the blog model and its functionality """
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = get_user_model().objects.create_user(
+            username='testuser',
+            email='test@user.com',
+            password='secret',
+        )
+
+        cls.post = Post.objects.create(
+            author=cls.user,
+            title='A test title',
+            body='Nice body content',
+        )
+
+    def test_post_model(self):
+        """ test to check if the created post is the exact one """
+        self.assertEqual(self.post.author.username, 'testuser')
+        self.assertEqual(self.post.title, 'A test title')
+        self.assertEqual(self.post.body, 'Nice body content')
+        self.assertEqual(str(self.post), 'A test title')
