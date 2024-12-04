f62fb8686bf63503e0b69ede3536fb2b9cf3e14e
Abenet Gebre <mail.micrq@proton.me>
2024-12-04 11:45:17 +0300

    added session and token authentication to the project

--- blogapi/blog/settings.py
+++ blogapi/blog/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'posts.apps.PostsConfig',
     # Third party
     'rest_framework',
+    'rest_framework.authtoken',
     'corsheaders',
 ]
 
@@ -49,7 +50,11 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
-    ]
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ],
 }
 
 MIDDLEWARE = [
