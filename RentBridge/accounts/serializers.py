43d0aee59a1a1773f62b333aa4003dc010db9529
Abenet Gebre <mail.micrq@proton.me>
2024-12-08 22:22:41 +0300

    created UserLoginView and UserLoginSerializer

--- RentBridge/accounts/serializers.py
+++ RentBridge/accounts/serializers.py
@@ -1,5 +1,6 @@
 """ Model serializer for the CustomUser model """
 from rest_framework import serializers
+from django.contrib.auth import authenticate
 from .models import CustomUser
 
 
@@ -15,4 +16,19 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         """ used to create a new user """
         user = CustomUser.objects.create_user(**validated_data)
+        return user
+
+
+class UserLoginSerializer(serializers.Serializer):
+    """ used to serialize the login data """
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        """ used to validate the login data """
+        email = data.get('email')
+        password = data.get('password')
+        user = authenticate(email=email, password=password)
+        if not user:
+            raise serializers.ValidationError('Invalid Credentials')
         return user
--- RentBridge/accounts/views.py
+++ RentBridge/accounts/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+""" views for account related endpoints """
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.authtoken import Token
+from .serializers import UserRegistrationSerializer, UserLoginSerializer
 
-# Create your views here.
+
+class UserRegistrationView(APIView):
+    """ api veiw for user registration """
+    def post(self, request):
+        """ used to create a new user """
+        serializer = UserRegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserLoginView(APIView):
+    """ api view for login """
+    def post(self, request):
+        """ used to login a user """
+        serializer = UserLoginSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.validated_data
+            token, _ = Token.objects.get_or_create(user=user)
+            return Response({'token': token.key}, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
