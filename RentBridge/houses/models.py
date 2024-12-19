2c82ce338f4529c44d3f67ceeb6e0d6bed51eeeb
Abenet Gebre <mail.micrq@proton.me>
2024-12-19 18:12:52 +0300

    created house model and migrated it to the database

---
+++ RentBridge/houses/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from rent_bridge.base import Base
+from django.contrib.auth import get_user_model
+
+
+class House(Base):
+    """ representation of a rental house """
+    owner = models.ForeignKey('accounts.CustomUser',
+                                 on_delete=models.CASCADE)
+    description = models.TextField()
+    location = models.ForeignKey('locations.Location',
+                                 on_delete=models.CASCADE)
+    number_of_room = models.IntegerField(default=1)
+    payment_method = models.ForeignKey('payments.PaymentMethod',
+                                       on_delete=models.CASCADE)
+    price_per_month = models.DecimalField(max_digits=10, decimal_places=2)
+    is_available = models.BooleanField(default=True)
+    video = models.FileField(upload_to='house_videos/',
+                             blank=True, null=True)
