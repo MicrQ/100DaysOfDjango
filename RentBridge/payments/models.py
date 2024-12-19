d10226fbc485570eb7e5a2f2c00699f3295c6bb2
Abenet Gebre <mail.micrq@proton.me>
2024-12-19 18:05:10 +0300

    created payment method model, wrote tests for it, and created migration and migrated it

--- RentBridge/payments/models.py
+++ RentBridge/payments/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from rent_bridge.base import Base
 
 
-class PaymentInterval(Base):
-    """ representation of the payment interval for a house """
+class PaymentMethod(Base):
+    """ representation of the payment methods for a house """
     name = models.CharField(max_length=50)
+
+    def __str__(self):
+        """ string representation of the payment method """
+        return self.name
